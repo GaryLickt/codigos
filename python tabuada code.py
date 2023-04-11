40fc07ce93e4a770b91ccd01f1d6c45e4dc55611
Gabriel (GabriBerp) <70419113+GaryLickt@users.noreply.github.com>
2023-04-11 09:01:04 -0300

    Update python tabuada code.py

--- python tabuada code.py
+++ python tabuada code.py
@@ -1,6 +1,6 @@
-numb = int(input("Digite um numero: \n"))
-max = (numb * 10) + 1
+numb = int(input("Digite um numero: \n")) #recebe um numero inteiro dado pelo usuario, e armazena ele em uma variavel chamada numb
+max = (numb * 10) + 1 #pega o numero dado, e calcula um maximo para ser usado como limite na tabuada
 print("==================================")
-for x in range(numb, max, numb):
-  print(str(numb) + " x " + str(x/numb) + " = " + str(x))
+for x in range(numb, max, numb): 
+  print(str(numb) + " x " + str(x/numb) + " = " + str(x)) #usando o for, e o range, conseguimos fazer um loop no print aonde toda linha ele vai printar "<numero dado> x <numero> = <valor>" ate chegar no maximo
 print("==================================")
