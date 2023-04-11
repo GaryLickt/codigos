640a62fe41e6b72d65301411374cc7db1dca0d6f
Gabriel (GabriBerp) <70419113+GaryLickt@users.noreply.github.com>
2023-04-11 08:52:19 -0300

    Update python hour code.py

--- python hour code.py
+++ python hour code.py
@@ -1,6 +1,6 @@
 import random
-hora = random.randint(1,24)
-min = random.randint(1,4) * 15
+hora = random.randint(1,24) #pega um valor aleatorio, pode remover esse random.randint(1,24) caso deseja inserir seus proprios numeros
+min = random.randint(1,4) * 15 #pega um valor aleatorio e multiplica por 15, para resultar em valores mais legiveis
 minHora = min/60
 horaTotal = hora + minHora
-print("A hora total é " + str(horaTotal) + "h" )
+print("A hora total é " + str(horaTotal) + "h" ) #printa o valor total de horas
