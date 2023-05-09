fc2505a82d97cf010fed39471dd859ef863fe73a
Gabriel (GabriBerp) <70419113+GaryLickt@users.noreply.github.com>
2023-05-09 08:15:35 -0300

    Jogo da adivinhação
    gera um numero aleatorio, e pede para o usuario adivinhar, a cada tentativa, da dica se o numero dado é maior ou menor que o escolhido

---
+++ python guess game.py
@@ -0,0 +1,12 @@
+import random 
+numero = random.randrange(1, 101)
+palpites = 0
+meu_palpite = int(input("Adivinhe meu número entre 1 e 100: "))
+while meu_palpite != numero:
+  palpites = palpites + 1
+  if meu_palpite > numero:
+    print(meu_palpite, "está acima.")
+  elif meu_palpite < numero:
+    print(meu_palpite, "está abaixo.")
+  meu_palpite = int(input("tente novamente: "))
+print("\nÓtimo, você acertou em", palpites, "tentativas!")
